10d64a926eed90f63f888e32706944e4862af14b
fablnt <fabio.lentini@mail.polimi.it>
2025-03-28 14:18:56 +0000

    updated test

--- singularity/test.py
+++ singularity/test.py
@@ -2,8 +2,7 @@ import time
 from datetime import datetime
 
 a = 0
-with open("time_log.txt", "a") as file:
-    while a<10:
-        a+=1
-        print(a)
-        time.sleep(1/2)
+while True:
+    a+=1
+    print(a)
+    time.sleep(1)
